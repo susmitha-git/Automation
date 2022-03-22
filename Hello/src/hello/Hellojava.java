22cd2300975de78f0ebbb82237960398d1b2f0fc
susmitha-git <ksusmitha228@gmail.com>
2022-03-22 10:15:08 +0530

    hello commit

---
+++ Hello/src/hello/Hellojava.java
@@ -0,0 +1,37 @@
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println("hello world");
+		
+	}
+		
+		
+		//int a=10;
+		//int b=20;
+		//int i,j;
+	//	if(a<b)
+		//{
+			//System.out.println("b is higher than a");
+		//}
+		//else
+		//{
+			//System.out.println("a is higher than b");
+		
+		//}
+		//for(i=1;i<=10;i++)
+		//{
+			//for(j=1;j<=10;j++);
+			//{
+	          // System.out.println("@");
+		//}
+
+
+		//}
+	//}
+	
+
+
